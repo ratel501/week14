435ced1398969b773abde90b60d9e7e448a63225
kyj77 <kyj7766@gmail.com>
2024-11-27 16:37:46 +0900

    week14

---
+++ src/FileCopyExample.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class FileCopyExample {
+    public static void main(String[] args) {
+        try (FileInputStream fis = new FileInputStream("source.txt");
+             FileOutputStream fos = new FileOutputStream("destination.txt")) {
+
+            int data;
+            while ((data = fis.read()) != -1) {
+                System.out.print((char) data);
+                fos.write(data);
+            }
+            System.out.println("File copied successfully!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
